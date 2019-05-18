8d8bc1752b5d396a1e1d0ddad47ec4158c5b7984
robmisasi <Rob.misasi@gmail.com>
2019-05-18 16:57:05 -0500

    Change run_training to use a data sampler for SGD

--- run_training.py
+++ run_training.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 from src.gan2 import MusicGAN
 from src.music_dataset import MusicDataset
+from torch.utils.data import RandomSampler
 
 data = MusicDataset('../MusicMats/smallest_training/')
+sampler = RandomSampler(data, replacement=True, num_samples=100)
 gan = MusicGAN('./new_check/','./new_out/', True)
-gan.train(data, 10000)
+gan.train(sampler, 10000)
